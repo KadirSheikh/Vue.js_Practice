e0d47dc9adc20a50b0554ce90c1c1cab314b9368
Kadir Sheikh <sheikhkadir02@gmail.com>
2022-12-03 13:25:50 +0530

    Added component and style

---
+++ vue_cli_first_project/vue_cli_first_app/src/components/FriendsContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ friends.name }}</h2>
+    <button @click="toggledetails">
+      {{ !showDetails ? "Show" : "Hide" }} Details
+    </button>
+
+    <ul v-if="showDetails">
+      <li><strong>Phone:</strong> {{ friends.phone }}</li>
+      <li><strong>Email:</strong>{{ friends.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showDetails: false,
+      friends: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "0123 45678 90",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggledetails() {
+      this.showDetails = !this.showDetails;
+    },
+  },
+};
+</script>
--- vue_cli_first_project/vue_cli_first_app/src/main.js
+++ vue_cli_first_project/vue_cli_first_app/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
-
+import FriendsContact from "./components/FriendsContact.vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.component("friends-contact", FriendsContact);
+
+app.mount("#app");
